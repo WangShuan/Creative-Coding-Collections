db89fe061795d268abfccdd2cad4f47d18781b44
WangShuan <loveabo103103@gmail.com>
2023-02-06 20:58:05 +0800

    fix text

--- forlooprect/mySketch.js
+++ forlooprect/mySketch.js
@@ -1,18 +1,20 @@
 function setup() {
 	createCanvas(windowWidth, windowHeight - 56);
 	background(0)
+	frameRate(10)
+	colorMode(HSB, 100)
 }
 
 let x = 60, y = 60
 function draw() {
 	let circleSize = 100, space = 10
-	fill(random(50, 255))
+	fill(255)
 	stroke(0, 50)
 	strokeWeight(10)
 	rectMode(CENTER)
 	rect(x, y, circleSize)
 	for (let i = 0; i < 60; i++) {
-		fill(random(0, 255), random(0, 255), map(mouseX + mouseY, 0, width + height, 0, 255))
+		fill(map(mouseX, 0, width, 0, 100), map(i, 0, 60, 50, 100), map(i, 0, 60, 50, 100))
 		noStroke()
 		circle(x + random(-40, 40), y - random(-40, 40), random(3, 10))
 	}
